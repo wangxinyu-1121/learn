a8b4932ccde99c180e7bf5b3002171b79e555666
王欣宇 <1778743575@qq.com>
2021-08-14 12:27:44 +0800

    fixed:修复不弹窗的问题

--- alien_invasion_game/alien_invasion.py
+++ alien_invasion_game/alien_invasion.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
 import pygame
+from pygame.locals import *
 from Settings import Settings
 
 
@@ -9,11 +12,11 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
-
+        #调用设置类
         self.settings = Settings()
 
         # self.screen = pygame.display.set_mode((1200, 800))
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode([self.settings.screen_width, self.settings.screen_height])
 
         pygame.display.set_caption("外星人入侵")
 
@@ -23,20 +26,23 @@ class AlienInvasion:
 
     def run_game(self):
         """开始游戏的循环"""
+        # 每次循环时都重绘屏幕
+        self.screen.fill(self.background_color)
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
         while True:
             # 监视键盘和鼠标事件
-            for user_event in pygame.event.get():
-                if user_event.type == pygame.QUIT:
-                    sys.q = exit()
-
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.background_color)
-
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+            # 更新屏幕内容
+            pygame.display.update()
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     # 创建游戏实例并运行游戏
     ai = AlienInvasion()
     ai.run_game()
+
---
+++ alien_invasion_game/test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import pygame
+from pygame.locals import *
+from Settings import Settings
+
+"""初始化游戏并创建游戏资源"""
+pygame.init()
+#调用设置类
+settings = Settings()
+
+# screen = pygame.display.set_mode((1200, 800))
+screen = pygame.display.set_mode([settings.screen_width, settings.screen_height])
+
+pygame.display.set_caption("外星人入侵")
+
+# 设置背景色
+# background_color = (230, 230, 230)
+background_color = settings.background_color
+
+
+"""开始游戏的循环"""
+# 每次循环时都重绘屏幕
+screen.fill(background_color)
+# 让最近绘制的屏幕可见
+pygame.display.flip()
+
+while True:
+    # 监视键盘和鼠标事件
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    # 更新屏幕内容
+    pygame.display.update()
+
