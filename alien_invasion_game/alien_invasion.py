ff64018da738ddf45ddc8acc2255d782f96e2a20
王欣宇 <1778743575@qq.com>
2021-08-15 17:35:49 +0800

    add:创建外星人群

--- alien_invasion_game/alien_invasion.py
+++ alien_invasion_game/alien_invasion.py
@@ -91,8 +91,27 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """创建外星人群"""
-        # 创建一个外星人
+        # 创建一个外星人并计算一行可容纳多少个外星人
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+        # 计算屏幕容纳多少行外星人
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - ship_height - (3 * alien_height))
+        number_rows = available_space_y // (2 * alien_height)
+        # 创建外星人群
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """创建一个外星人并将其放在当前行"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
