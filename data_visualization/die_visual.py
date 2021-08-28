c8a2beae06717fabc891d41d914e56f82e657272
王欣宇 <1778743575@qq.com>
2021-08-28 19:06:27 +0800

    add:1个D6,1个D8

--- data_visualization/die_visual.py
+++ data_visualization/die_visual.py
@@ -3,13 +3,13 @@ from plotly import offline
 
 from die import Die
 
-# 创建两个D6骰子
+# 创建两个不同的骰子
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(8)
 
 # 投掷几次骰子并将结果存储在列表中
 results = []
-for roll_num in range(1000):
+for roll_num in range(5000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -28,6 +28,6 @@ data = [Bar(x=x_values, y=frequencies)]
 # dtick指定了x轴显示的刻度间距
 x_axis_config = {'title': '结果', 'dtick': 1}
 y_axis_config = {'title': '结果频次'}
-my_layout = Layout(title='投1000次2个D6骰子的结果', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='two_d6.html')
+my_layout = Layout(title='投5000次1个D6和一个D8骰子的结果', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d8.html')
 
