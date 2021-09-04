78ada4118033f856c6d9f2b56ff403cb26e937c7
王欣宇 <1778743575@qq.com>
2021-09-04 18:44:58 +0800

    add:web应用程序-从django入手，创建页面

--- web_application/learning_log/learning_logs/urls.py
+++ web_application/learning_log/learning_logs/urls.py
@@ -7,4 +7,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # 主页
     path('', views.index, name='index'),
+    # 显示所有主题
+    path('topics/', views.topics, name='topics'),
+    # 显示特定主体的详细页面
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
