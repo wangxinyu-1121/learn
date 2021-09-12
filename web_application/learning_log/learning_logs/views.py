5838bf3b00fad333d78cd86694158f83c842444b
王欣宇 <1778743575@qq.com>
2021-09-12 12:43:41 +0800

    add:web应用程序-从django入手，创建用户账户

--- web_application/learning_log/learning_logs/views.py
+++ web_application/learning_log/learning_logs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import Topic
-from .forms import TopicForm,EntryForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
@@ -9,21 +12,27 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """显示所有主题"""
-    topics = Topic.objects.order_by('date_added')
+    # topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
-
+@login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求的主题属于当前用户
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
-
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
@@ -33,13 +42,16 @@ def new_topic(request):
         # POST提交的数据：对数据进行处理
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            # form.save()
             return redirect('learning_logs:topics')
     # 显示空表单或者支出表单数据无效
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
-
+@login_required
 def new_entry(request, topic_id):
     """在特定主题下添加新条目"""
     topic = Topic.objects.get(id=topic_id)
@@ -58,4 +70,27 @@ def new_entry(request, topic_id):
     # 显示空表单或者支出表单数据无效
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+@login_required
+def edit_entry(request, entry_id):
+    """编辑已有条目"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
+
+    if request.method != 'POST':
+        # 初次请求：使用当前条目填充表单
+        form = EntryForm(instance=entry)
+    else:
+        # POST提交的数据：对数据进行处理
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
 
---
+++ web_application/learning_log/users/urls.py
@@ -0,0 +1,12 @@
+"""为应用程序users定义URL模式"""
+
+from django.urls import path, include
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # 包含默认的身份验证 URL
+    path('', include('django.contrib.auth.urls')),
+    # 注册页面
+    path('register/', views.register, name='register'),
+]
